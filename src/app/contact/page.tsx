a0d9d250d715ef4c8ed01b529f4ef8e8de56c0e7
ksnu-lab <dgkwon.ktr@gmail.com>
2026-02-16 18:10:21 +0900

    Remove files

--- src/app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getAllFaqItems } from "@/lib/mdx";
-import SupportPageClient from "./SupportPageClient";
-
-export default function ContactPage() {
-  const faqItems = getAllFaqItems();
-
-  return <SupportPageClient faqItems={faqItems} />;
-}
--- src/app/news/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getAllNewsPosts } from "@/lib/mdx";
-import NewsListClient from "./NewsListClient";
-
-export default function NewsPage() {
-  const posts = getAllNewsPosts();
-
-  return <NewsListClient posts={posts} />;
-}
