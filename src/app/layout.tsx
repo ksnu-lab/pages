880e289e8727643340f60f49bce508667111dded
ksnu-lab <dgkwon.ktr@gmail.com>
2026-02-19 01:37:21 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import ClientLayout from "@/components/layout/ClientLayout";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -20,6 +21,17 @@ export default function RootLayout({
     <html lang="ko">
       <body className="antialiased min-h-screen flex flex-col">
         <ClientLayout>{children}</ClientLayout>
+
+        {/* Microsoft Clarity 추적 코드 삽입 */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vjdvdgo52l");
+          `}
+        </Script>
       </body>
     </html>
   );
